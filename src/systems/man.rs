3e0692a2400f74ffe612cea34bccb2474ea2aea4
= <=>
2019-08-25 19:00:33 +0100

    slight tidy up of movement calculation in man system

--- src/systems/man.rs
+++ src/systems/man.rs
@@ -1,4 +1,5 @@
 use amethyst::core::Transform;
+use amethyst::core::math::{Vector3};
 use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
 use amethyst::input::{InputHandler, StringBindings};
 
@@ -19,32 +20,22 @@ impl<'s> System<'s> for ManSystem {
                 Player::Player1 => (input.axis_value("player1_x"), input.axis_value("player1_y")),
                 Player::Player2 => (input.axis_value("player2_x"), input.axis_value("player2_y")),
             };
-            if let Some(mv_amount) = movement_x {
-                if mv_amount != 0.0 {
-                    let scaled_amount = 1.2 * mv_amount as f32;
-                    let man_x = transform.translation().x;
-                    transform.set_translation_x(
-                        (man_x + scaled_amount)
-                            .min(SCREEN_WIDTH - MAN_WIDTH * 0.5)
-                            .max(MAN_WIDTH * 0.5),
-                    );
-                    transform.prepend_translation_x(scaled_amount);
-                    println!("moving {}", mv_amount);
-                }
-            }
-            if let Some(mv_amount) = movement_y {
-                if mv_amount != 0.0 {
-                    let scaled_amount = 1.2 * mv_amount as f32;
-                    let man_y = transform.translation().y;
-                    transform.set_translation_y(
-                        (man_y + scaled_amount)
-                            .min(SCREEN_HEIGHT - MAN_HEIGHT * 0.5)
-                            .max(MAN_HEIGHT * 0.5),
-                    );
-                    transform.prepend_translation_y(scaled_amount);
-                    println!("moving {}", mv_amount);
-                }
-            }
+
+            let translation_x = get_new_pos(movement_x, transform.translation().x);
+            let translation_y = get_new_pos(movement_y, transform.translation().y);
+            transform.set_translation(Vector3::new(translation_x, translation_y, 0.0 as f32));
         }
     }
+}
+
+fn get_new_pos(movement: Option<f32>, starting_translation: f32) -> f32 {
+    if let Some(mv_amount) = movement {
+        if mv_amount != 0.0 {
+            let scaled_amount = 1.2 * mv_amount as f32;
+            return (starting_translation + scaled_amount)
+                     .min(SCREEN_HEIGHT - MAN_HEIGHT * 0.5)
+                     .max(MAN_HEIGHT * 0.5);
+        }
+    }
+    return starting_translation;
 }
